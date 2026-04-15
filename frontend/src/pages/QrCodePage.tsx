a31c1137fd8ef99c296a73e09b3fccee82af1f8f
0000masa <134136756+0000masa@users.noreply.github.com>
2026-04-15 21:33:39 +0900

    QRコードを非同期生成するコンポーネントを作成

--- frontend/src/pages/QrCodePage.tsx
+++ frontend/src/pages/QrCodePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import QrCodeGenerator from '../components/QrCodeGenerator';
+import QrCodeAsyncGenerator from '../components/QrCodeAsyncGenerator';
 import QrCodeList from '../components/QrCodeList';
 
 const QrCodePage: React.FC = () => {
   return (
     <Layout>
       <QrCodeGenerator />
+      <QrCodeAsyncGenerator />
       <QrCodeList />
     </Layout>
   );
